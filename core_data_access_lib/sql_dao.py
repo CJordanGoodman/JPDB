0c23606cd20772cc9f25b3b1f34983f380ceb403
Matt <mmdstanley@gmail.com>
2021-08-15 00:43:40 -0600

    Added decorator for sqlite3, so all other functions can leverage it

--- core_data_access_lib/sql_dao.py
+++ core_data_access_lib/sql_dao.py
@@ -4,27 +4,33 @@ from entities import *
 # NOTE: This script will not work unless the jp.db sqlite database has been set up as-per 
 # https://github.com/JapaneseDatabase/JPDB/wiki/SQLite-Setup-Guide
 
+### Decorators ###
+__DATABASE_NAME = 'jp.db'
+
+def sqlDecorator():
+	def deco(func):
+		def wrapper(*args, **kwargs):
+			conn = sqlite3.connect(__DATABASE_NAME) # Connect to the database
+			cursor = conn.cursor() # Create cursor object
+			func(cursor, *args, **kwargs) # Call function with cursor
+			conn.commit() # Commit any changes to the database
+			conn.close() # Close the current connection to the database
+		return wrapper
+	return deco
+
+
 ### KANJI EDIT METHODS ###
 
-# TODO: Define method
-def add_kanji(kanji_list):
-	conn = sqlite3.connect('jp.db')
-	cursor = conn.cursor()
-
+@sqlDecorator
+def add_kanji(cursor, kanji_list):
 	execute_string = "INSERT INTO kanji (enc_char, onyomi_pros, kunyomi_pros, prim_eng_def, alt_eng_defs, " \
 					 "ex_words, jlpt_lvl, ex_lit_sent, ex_fig_sent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
 
 	for kanji in kanji_list:
 		cursor.execute(execute_string, kanji.return_list())
 
-	conn.commit()
-	conn.close()
-
-# TODO: Define method
-def edit_kanji(kanji_list):
-	conn = sqlite3.connect('jp.db')
-	cursor = conn.cursor()
-
+@sqlDecorator
+def edit_kanji(cursor, kanji_list):
 	execute_string = "UPDATE kanji SET onyomi_pros = ?, kunyomi_pros = ?, prim_eng_def = ?, alt_eng_defs = ?, " \
 					 "ex_words = ?, jlpt_lvl = ?, ex_lit_sent = ?, ex_fig_sent = ? WHERE enc_char = ?"
 
@@ -33,30 +39,18 @@ def edit_kanji(kanji_list):
 		kanji_values.append(kanji_values.pop(0)) # Move first value to the end
 		cursor.execute(execute_string, kanji_values)
 
-	conn.commit()
-	conn.close()
-
-# TODO: Define method
-def delete_kanji(kanji_list):
-	conn = sqlite3.connect('jp.db')
-	cursor = conn.cursor()
-
+@sqlDecorator
+def delete_kanji(cursor, kanji_list):
 	execute_string = "DELETE FROM kanji WHERE enc_char = ?"
 
 	for kanji in kanji_list:
 		cursor.execute(execute_string, (kanji.enc_character,))
 
-	conn.commit()
-	conn.close()
-
 
 ### VOCAB EDIT METHODS ###
 
-# TODO: Define method
-def add_vocab(vocab_list):
-	conn = sqlite3.connect('jp.db')
-	cursor = conn.cursor()
-
+@sqlDecorator
+def add_vocab(cursor, vocab_list):
 	execute_string = "INSERT INTO vocab (enc_term, pros, prim_eng_def, alt_eng_defs, " \
 					 "comp_kanji, part_of_speech, transitivity, ex_lit_sent, ex_fig_sent) " \
 					 "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
@@ -64,14 +58,8 @@ def add_vocab(vocab_list):
 	for vocab in vocab_list:
 		cursor.execute(execute_string, vocab.return_list())
 
-	conn.commit()
-	conn.close()
-
-# TODO: Define method
-def edit_vocab(vocab_list):
-	conn = sqlite3.connect('jp.db')
-	cursor = conn.cursor()
-
+@sqlDecorator
+def edit_vocab(cursor, vocab_list):
 	execute_string = "UPDATE vocab SET pros = ?, prim_eng_def = ?, alt_eng_defs = ?, " \
 					 "comp_kanji = ?, part_of_speech = ?, transitivity = ?, ex_lit_sent = ?, ex_fig_sent = ? " \
 					 "WHERE enc_term = ?"
@@ -81,98 +69,75 @@ def edit_vocab(vocab_list):
 		vocab_values.append(vocab_values.pop(0)) # Move first value to the end
 		cursor.execute(execute_string, vocab_values)
 
-	conn.commit()
-	conn.close()
-
-# TODO: Define method
-def delete_vocab(vocab_list):
-	conn = sqlite3.connect('jp.db')
-	cursor = conn.cursor()
-
+@sqlDecorator
+def delete_vocab(cursor, vocab_list):
 	execute_string = "DELETE FROM vocab WHERE enc_term = ?"
 
 	for vocab in vocab_list:
 		cursor.execute(execute_string, (vocab.enc_vocab,))
 
-	conn.commit()
-	conn.close()
-
 
 ### USER EDIT METHODS ###
 
 # TODO: Define method
-def add_user(user):
+@sqlDecorator
+def add_user(cursor, user):
 	raise NotImplementedError
 
 # TODO: Define method
-def delete_user(user):
+@sqlDecorator
+def delete_user(cursor, user):
 	raise NotImplementedError
 
 
 ### TAG EDIT METHODS ###
 
 # TODO: Define method
-def add_tag(tag_id, username, term_id, term_type):
+@sqlDecorator
+def add_tag(cursor, tag_id, username, term_id, term_type):
 	raise NotImplementedError
 
 # TODO: Define method
-def delete_tag(tag_id, username, term_id, term_type):
+@sqlDecorator
+def delete_tag(cursor, tag_id, username, term_id, term_type):
 	raise NotImplementedError
 
 
 ### SEARCH METHODS ###
 
-# TODO: Clean up method result
-def get_terms_with_tag(tag_id, username, term_type):
-	conn = sqlite3.connect('jp.db')
-	cursor = conn.cursor()
-
+@sqlDecorator
+def get_terms_with_tag(cursor, tag_id, username, term_type):
 	if term_type == Tag.TermType.KANJI:
-		execute_string = "SELECT * FROM tag LEFT JOIN kanji ON tag.term_id = kanji.kanji_id WHERE " \
-		"tag.term_type = \"kanji\" tag.username = \"{0}\", tag.tag_id = \"{1}\"".format(username, tag_id)
+		execute_string = 'SELECT * FROM tag LEFT JOIN kanji ON tag.term_id = kanji.kanji_id WHERE ' \
+		'tag.term_type = "kanji" tag.username = "?", tag.tag_id = "?"'
 	elif term_type == Tag.TermType.VOCAB:
-		execute_string = "SELECT * FROM tag LEFT JOIN vocab ON tag.term_id = vocab.term_id WHERE " \
-		"tag.term_type = \"vocab\" tag.username = \"{0}\", tag.tag_id = \"{1}\"".format(username, tag_id)
+		execute_string = 'SELECT * FROM tag LEFT JOIN vocab ON tag.term_id = vocab.term_id WHERE ' \
+		'tag.term_type = "vocab" tag.username = "?", tag.tag_id = "?"'
 
-	result = cursor.execute(execute_string).fetchall()[0]
+	return cursor.execute(execute_string, (username, tag_id)).fetchall()
 
-	conn.commit()
-	conn.close()
+@sqlDecorator
+def get_tags_for_user(cursor, username):
+	execute_string = 'SELECT DISTINCT tag_id FROM tag WHERE tag.username = "?"'
 
-	return result
+	return cursor.execute(execute_string, (username,)).fetchall()
 
-# TODO: clean up method result
-def get_tags_for_user(username):
-	conn = sqlite3.connect('jp.db')
-	cursor = conn.cursor()
-
-	execute_string = "SELECT DISTINCT tag_id FROM tag WHERE tag.username = \"{0}\"".format(username)
-
-	result = cursor.execute(execute_string).fetchall()
-
-	conn.commit()
-	conn.close()
-
-	return result
-
-def get_term(enc_term, term_type):
-	conn = sqlite3.connect('jp.db')
-	cursor = conn.cursor()
-
+@sqlDecorator
+def get_term(cursor, enc_term, term_type):
 	if term_type == Tag.TermType.KANJI:
-		execute_string = "SELECT * FROM kanji WHERE enc_char = \"{0}\"".format(enc_term)
+		execute_string = 'SELECT * FROM kanji WHERE enc_char = "?"'
 	elif term_type == Tag.TermType.VOCAB:
-		execute_string = "SELECT * FROM vocab WHERE enc_term = \"{0}\"".format(enc_term)
+		execute_string = 'SELECT * FROM vocab WHERE enc_term = "?"'
 
-	result = cursor.execute(execute_string).fetchall()[0]
+	listResults = cursor.execute(execute_string, (enc_term,)).fetchall()
 
-	conn.commit()
-	conn.close()
-
-	if term_type == Tag.TermType.KANJI:
-		return convert_sql_string_to_kanji(result)
-	elif term_type == Tag.TermType.VOCAB:
-		return convert_sql_string_to_vocab(result)
+	convResults = []
+	for result in listResults:
+		if term_type == Tag.TermType.KANJI:
+			convResults.append(convert_sql_string_to_kanji(result))
+		elif term_type == Tag.TermType.VOCAB:
+			convResults.append(convert_sql_string_to_vocab(result))
+	return convResults
 
 
 def convert_sql_string_to_kanji(row):
